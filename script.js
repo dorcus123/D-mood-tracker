04320fd43dbf60a1f5d691e2a2e65e49795f5dd5
Dorcus <dorcusnagadya64@gmail.com>
2024-12-19 11:41:54 -0800

    Initial commit - My mood tracker project

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.getElementById("submit-btn").addEventListener("click", function () {
 
     switch (mood) {
         case "happy":
-            message = "Savor every second of this happiness, allowing it to spread. You are on a path of positivity and growth, keep moving forward!";
+            message = "Favor every second of this happiness, allowing it to spread. You are on a path of positivity and growth, keep moving forward!";
             break;
         case "sad":
             message = "Even when times are tough, keep in mind that brighter days are ahead. Take it one day, minute, second at a time... each moment will bring you closer to healing!";
@@ -15,7 +15,7 @@ document.getElementById("submit-btn").addEventListener("click", function () {
             message = "One step at a time. You do not have to have it all figured out right now. Remember to breathe... ground yourself in this moment and know that you will get through this too!";
             break;
         case "excited":
-            message = "Embrace your enthusiasm and allow it to fuel your dreams. The universe is giving you a reason to smile! This energy will lead you far on your path!";
+            message = "Embrace your enthusiasm and allow it to fuel your dreams. God is giving you a reason to smile! This energy will lead you far on your path!";
             break;
         default:
             message = "Please select a mood to receive a motivational message.";
